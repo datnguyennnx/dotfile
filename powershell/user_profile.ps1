bd20127d17d31d070460a010364b253b20f2ca60
Nguyen Dat <official.nguyendat@gmail.com>
2023-07-26 18:18:07 +0700

    Update config Powershell

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -14,16 +14,17 @@ Import-Module posh-git
 Import-Module -Name Terminal-Icons
 Import-Module -Name PSReadLine
 
-Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineOption -BellStyle None
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineKeyHandler -Key Tab -Function Complete
+Set-PSReadLineKeyHandler -Chord "RightArrow" -Function ForwardWord
+Set-PSReadLineKeyHandler -Chord 'Ctrl+Spacebar' -Function MenuComplete
 
 # Alias
 Set-Alias ll ls
 Set-Alias touch ni 
-Set-Alias vim nvim
+Set-Alias vim nvim  
 # Utilities
 function which ($command) {
 	Get-Command -Name $command -ErrorAction SilentlyContinue | 
